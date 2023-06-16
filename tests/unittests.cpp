b8e781a6a77fcba107aba113a59591933699df52
Sebastian Lindner <sebastian.lindner@tuhh.de>
2023-06-16 15:20:42 +0200

    add readme, LGPL license and copyright notices

--- tests/unittests.cpp
+++ tests/unittests.cpp
@@ -1,6 +1,18 @@
+// The L-Band Digital Aeronautical Communications System (LDACS) Radio Link Control (RLC) implements the RLC protocol for the LDACS Air-Air Medium Access Control simulator.
+// Copyright (C) 2023  Sebastian Lindner, Konrad Fuger, Musab Ahmed Eltayeb Ahmed, Andreas Timm-Giel, Institute of Communication Networks, Hamburg University of Technology, Hamburg, Germany
 //
-// Created by Konrad Fuger on 03.12.20.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <cppunit/ui/text/TestRunner.h>
 #include <cppunit/TestResult.h>
